630dfb8eddc5ae6ad9b12e781f80da69f00aefbe
Sylvanas <ladysylvanas@yahoo.com>
2012-05-19 23:32:26 +0300

    added alerts

--- client/default/app/app.js
+++ client/default/app/app.js
@@ -6,19 +6,22 @@ alert('alertFromapp');
 
 
 Ext.application({
+	
 	name: 'App',  
 
 	initialize: function () {
+		alert('0');
 		this.callParent();
 	},	
 	launch: function() {
+		alert('1');
 		App.viewport = Ext.create('Ext.Panel', {
 			fullscreen: true,
 			layout: 'hbox',
             items: [{xtype: 'button' , text: 'asdfasfd'}]
 		});
-		alert('sdf2');
+		alert('2');
     },
     
 });
-
+alert('3');
